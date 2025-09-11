ffc8f8ccb9a70a6dd4245f82b0f6439e5a8257bb
agren00 <agrenhesronnufa@gmail.com>
2025-09-11 10:09:38 +0700

    update file react project

---
+++ bab 2 react/js_module/haloJakarta.js
@@ -0,0 +1,12 @@
+// export const haloJakarta = () => {
+//   return "Halo Jakarta"
+// }   
+
+//Alternatif cara penulisan lain adalah membuat perintah export terpisah:
+
+const haloJakarta = () => {
+  return "Halo Jakarta";
+}
+
+//export {haloJakarta}; //bisa pake ini atau pake yg dibawah
+export default haloJakarta;
